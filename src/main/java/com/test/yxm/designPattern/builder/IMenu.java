2a5667afce9af4659b1296ebabb31639b9a6249a
yxm <2445826266@qq.com>
2021-11-03 19:40:09 +0800

    建造者模式

---
+++ src/main/java/com/test/yxm/designPattern/builder/IMenu.java
@@ -0,0 +1,13 @@
+package com.test.yxm.designPattern.builder;
+
+public interface IMenu {
+    IMenu appendCeiling(Matter matter); // 吊顶
+
+    IMenu appendCoat(Matter matter);    // 涂料
+
+    IMenu appendFloor(Matter matter);   // 地板
+
+    IMenu appendTile(Matter matter);    // 地砖
+
+    String getDetail();                 // 明细
+}
---
+++ src/main/java/com/test/yxm/designPattern/builder/Test.java
@@ -0,0 +1,15 @@
+package com.test.yxm.designPattern.builder;
+
+public class Test {
+
+    @org.junit.Test
+    public void m1(){
+        Builder builder = new Builder();
+        // 豪华欧式
+        System.out.println(builder.levelOne(132.52D).getDetail());
+        // 轻奢田园
+        System.out.println(builder.levelTwo(98.25D).getDetail());
+        // 现代简约
+        System.out.println(builder.levelThree(85.43D).getDetail());
+    }
+}
