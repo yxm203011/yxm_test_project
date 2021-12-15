3a810ac33459e01b7e75a49720934675872b5c8e
yxm <2445826266@qq.com>
2021-12-15 18:19:06 +0800

    111

--- src/main/java/com/test/yxm/test.java
+++ src/main/java/com/test/yxm/test.java
@@ -1,23 +1,26 @@
 package com.test.yxm;
 
+import java.io.*;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.nio.charset.StandardCharsets;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class test {
 
     public static void main(String[] args) throws Exception {
-        InvocationHandler invocationHandler = new InvocationHandler() {
-            @Override
-            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                System.out.println(method);
-                if(method.getName().equals("getName")){
-                    System.out.println("名字："+args[0]);
-                }
-                return null;
-            }
-        };
-        aaa a = (aaa)Proxy.newProxyInstance(aaa.class.getClassLoader(),new Class[]{aaa.class},invocationHandler);
-        a.getName("aaaaaa");
+        // 当前时间:
+        Calendar c = Calendar.getInstance();
+        c.setTime(new Date());
+        c.add(Calendar.MINUTE,-6);
+        c.add(Calendar.SECOND, -60);
+        c.add(Calendar.HOUR_OF_DAY,-2);
+        c.add(Calendar.YEAR,-1);
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(c.getTime()));
     }
 }
