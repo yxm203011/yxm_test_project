da4f3a8392c491e3f047ca5d501b03a3cecf6d98
yxm <2445826266@qq.com>
2022-02-09 18:07:35 +0800

    111

--- src/main/java/com/test/yxm/test.java
+++ src/main/java/com/test/yxm/test.java
@@ -1,26 +1,22 @@
 package com.test.yxm;
 
-import java.io.*;
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
-import java.nio.charset.StandardCharsets;
-import java.text.NumberFormat;
-import java.text.SimpleDateFormat;
-import java.util.*;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
 public class test {
 
     public static void main(String[] args) throws Exception {
-        // 当前时间:
-        Calendar c = Calendar.getInstance();
-        c.setTime(new Date());
-        c.add(Calendar.MINUTE,-6);
-        c.add(Calendar.SECOND, -60);
-        c.add(Calendar.HOUR_OF_DAY,-2);
-        c.add(Calendar.YEAR,-1);
-        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(c.getTime()));
+        System.out.println("Thread Start");
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(5000l);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("线程中的方法");
+            }
+        };
+        thread.start();
+        System.out.println("Thread End");
     }
 }
---
+++ src/main/java/com/test/yxm1/tast.java
@@ -0,0 +1,10 @@
+package com.test.yxm1;
+
+public class tast {
+
+    /*public static void main(String[] args) {
+        System.out.println("1");
+    }*/
+    //public:关键字是一种访问修饰符，代表可见性。这意味着所有人都可以看到它。
+    //statis:
+}
