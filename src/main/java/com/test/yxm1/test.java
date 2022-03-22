e288ca22db494323cf9a93cac7e8f226b505e8be
yxm <2445826266@qq.com>
2022-03-22 14:05:26 +0800

    callable和线程池

--- src/main/java/com/test/yxm1/test.java
+++ src/main/java/com/test/yxm1/test.java
@@ -1,5 +1,9 @@
 package com.test.yxm1;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.*;
 
 public class test {
@@ -288,6 +292,51 @@ public class test {
             return name + ":" + returnNum;
         }
     }*/
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = new ServerSocket(9999);
+        System.out.println("service is running....");
+        for(;;){
+            Socket sock = ss.accept();
+            System.out.println("connected from "+sock.getRemoteSocketAddress());
+            Thread t = new Handler(sock);
+            t.start();
+        }
+    }
 
-
+    static class Handler extends Thread{
+        Socket sock;
+        public Handler(Socket sock){
+            this.sock = sock;
+        }
+        @Override
+        public void run(){
+            try(InputStream input = this.sock.getInputStream()){
+                try(OutputStream output = this.sock.getOutputStream()){
+                    handle(input,output);
+                }
+            }catch(Exception e){
+                try{
+                    this.sock.close();
+                }catch(IOException ioe){
+                }
+                System.out.println("clint disconnected");
+            }
+        }
+    }
+    private static void handle(InputStream input,OutputStream output) throws IOException{
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(output, StandardCharsets.UTF_8));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8));
+        writer.write("hello\n");
+        writer.flush();
+        for (;;){
+            String s = reader.readLine();
+            if(s.equals("bye")){
+                writer.write("bye\n");
+                writer.flush();
+                break;
+            }
+            writer.write("ok:"+s+"\n");
+            writer.flush();
+        }
+    }
 }
