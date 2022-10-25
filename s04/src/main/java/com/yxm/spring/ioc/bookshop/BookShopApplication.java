2aa74dae69820b0364d6122203f3bf249e9ded98
yxm <2445826266@qq.com>
2022-10-25 17:48:01 +0800

    使用context.getBeanDefinitionNames获取bean的信息

--- s04/src/main/java/com/yxm/spring/ioc/bookshop/BookShopApplication.java
+++ s04/src/main/java/com/yxm/spring/ioc/bookshop/BookShopApplication.java
@@ -1,9 +1,12 @@
 package com.yxm.spring.ioc.bookshop;
 
 import com.yxm.spring.ioc.bookshop.entity.Company;
+import com.yxm.spring.ioc.bookshop.entity.Computer;
 import com.yxm.spring.ioc.bookshop.service.BookService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Arrays;
 
 public class BookShopApplication {
     public static void main(String[] args) {
@@ -14,5 +17,16 @@ public class BookShopApplication {
         Company company = context.getBean("company", Company.class);
         String name = company.getInfo().getProperty("name");
         System.out.println(company.toString()+"name:"+name);
+        System.out.println("-------------------");
+        String[] beanNames = context.getBeanDefinitionNames();
+        for (String beanName : beanNames) {
+            Object bean = context.getBean(beanName);
+            System.out.println("类型:"+bean.getClass().getName());
+            System.out.println("内容:"+bean);
+        }
+
+        System.out.println("------------------");
+        Computer bean = context.getBean("com.yxm.spring.ioc.bookshop.entity.Computer#0", Computer.class);
+        System.out.println(bean.getBrand());
     }
 }
